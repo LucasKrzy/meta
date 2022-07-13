6dd8699529885fc21dfbffcf1fc30bc282347186
LucasKrzy <lucaskrzy@gmail.com>
2022-07-13 11:23:07 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="Meta" />
+            <h1>Meta</h1>
+            <p>
+              Desenvolvido por &nbsp;
+              <a href="https://github.com/LucasKrzy">@LucasKrzy</a>
+            </p>
+        </div>
+    </header>
+    )
+}
+
+export default Header
